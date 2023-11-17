6cae1cf9fc4ab8f8fc66db61df7402a66f3cbea8
João Luiz <jluizmineiro@gmail.com>
2023-11-17 15:06:57 -0300

    operador logico &&

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -9,7 +9,7 @@ export const Card = ({ phrase, author }: props) => {
   return (
     <div className="w-96 border-2 border-red-600 p-3 text-3xl text-center italic">
       <h3 className="text-3xl font-bold italic">{phrase}</h3>
-      <p className="text-right text-sm">{author ?? "autor desconhecido"}</p>
+      {author && <p className="text-right text-sm">{author}</p>}
     </div>
   );
 };
