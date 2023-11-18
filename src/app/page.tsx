91b3e94676217a84cb04ba2e40cf045ab832738b
João Luiz <jluizmineiro@gmail.com>
2023-11-17 21:48:16 -0300

    filtrando listas

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,28 @@
-import { Card } from "@/components/Card";
+import { peopleList } from "@/data/peopleList";
 
 export default function page() {
+  const engenheiros = peopleList.filter(
+    (person) => person.profession === "Engenheira de Software"
+  );
+
   return (
     <div>
       <h1 className="font-bold text-2xl">Ola mundo</h1>
-      <h3>João Luiz</h3>
-
-      <Card phrase="Alguma frase TOP!!!" author="Coach de milhões" />
+      <h3 className="font-black">João Luiz</h3>
+      {engenheiros.length > 0 && (
+        <>
+          <h3 className="font-semibold text-red-600 text-lg">
+            Lista de engenheiros de software:
+          </h3>
+          <ul>
+            {engenheiros.map((person) => (
+              <li key={person.id}>
+                {person.name} - {person.profession}
+              </li>
+            ))}
+          </ul>
+        </>
+      )}
     </div>
   );
 }
---
+++ src/data/peopleList.ts
@@ -0,0 +1,11 @@
+import { Person } from "@/types/person";
+
+export const peopleList: Person[] = [
+  { id: 1, name: "Maria", profession: "Engenheira de Software" },
+  { id: 2, name: "João", profession: "Médico" },
+  { id: 3, name: "Ana", profession: "Professora" },
+  { id: 4, name: "Pedro", profession: "Advogado" },
+  { id: 5, name: "Clara", profession: "Psicóloga" },
+  { id: 6, name: "Sofia", profession: "Engenheira de Software" },
+  { id: 7, name: "Bia", profession: "Engenheira de Software" },
+];
