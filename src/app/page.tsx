dc056263aab872102b062f5c32931fd19df4ee91
luizmineiro <jluizmineiro@gmail.com>
2023-12-19 18:59:09 -0300

    Exercício: Greeting condicional 2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,18 +9,22 @@ export default function page() {
   const hour = new Date().getHours();
   let greeting = "";
 
-  if (hour > 0 && hour < 12) {
-    greeting = "Bom dia 😁";
-  } else if (hour >= 12 && hour < 18) {
-    greeting = "Boa tarde 😎";
-  } else if (hour >= 18 && hour <= 23) {
-    greeting = "Boa noite 😴"
-  }
+  // if (hour > 0 && hour < 12) {
+  //   greeting = "Bom dia 😁";
+  // } else if (hour >= 12 && hour < 18) {
+  //   greeting = "Boa tarde 😎";
+  // } else if (hour >= 18 && hour <= 23) {
+  //   greeting = "Boa noite 😴"
+  // }
 
   return (
     <div className="w-screen h-screen flex flex-col justify-center items-center text-white bg-gradient-to-r from-sky-500 to-indigo-500">
       <div className="text-9xl">{fullTime}</div>
-      <div className="text-5xl">{greeting}</div>
+      <div className="text-5xl">
+        {hour > 0 && hour < 12 && "Bom dia 😁"}
+        {hour >= 12 && hour < 18 && "Boa tarde 😎"}
+        {hour >= 18 && hour <= 23 && "Boa noite 😴"}
+      </div>
     </div>
   );
 }
