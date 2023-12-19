95b485638d9e49a2885ffd1a6ff8710af79f7d5c
luizmineiro <jluizmineiro@gmail.com>
2023-12-19 20:21:00 -0300

    Rating via emoji(facil)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,30 +1,9 @@
-import { peopleList } from "@/data/peopleList";
+import { EmojiRating } from "@/components/EmojiRating";
 
 export default function page() {
-  const fullTime = new Intl.DateTimeFormat("pt-BR", {
-    timeStyle: "short",
-    hour12: false,
-  }).format();
-
-  const hour = new Date().getHours();
-  let greeting = "";
-
-  // if (hour > 0 && hour < 12) {
-  //   greeting = "Bom dia 😁";
-  // } else if (hour >= 12 && hour < 18) {
-  //   greeting = "Boa tarde 😎";
-  // } else if (hour >= 18 && hour <= 23) {
-  //   greeting = "Boa noite 😴"
-  // }
-
   return (
-    <div className="w-screen h-screen flex flex-col justify-center items-center text-white bg-gradient-to-r from-sky-500 to-indigo-500">
-      <div className="text-9xl">{fullTime}</div>
-      <div className="text-5xl">
-        {hour > 0 && hour < 12 && "Bom dia 😁"}
-        {hour >= 12 && hour < 18 && "Boa tarde 😎"}
-        {hour >= 18 && hour <= 23 && "Boa noite 😴"}
-      </div>
+    <div className="w-screen h-screen flex justify-center items-center">
+      <EmojiRating rate ={3.2}/>
     </div>
   );
 }
